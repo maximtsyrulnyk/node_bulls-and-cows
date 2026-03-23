bb28877feb4d16eb4363153823b31b6e88989244
Maksym Tsyrulnyk <maximtsyrulnyk@gmail.com>
2026-03-23 20:32:16 +0200

    make some changes

--- src/app.js
+++ src/app.js
@@ -8,22 +8,24 @@ function startGame() {
   const secret = generateSecretNumber();
   let isGuessed = false;
 
+  // console.log('Welcome to Bulls and Cows!');
+
   while (!isGuessed) {
     const input = readline.question('Enter your 4-digit guess: ');
 
     if (!checkIsValidUserInput(input) || checkIsRepeated(input)) {
       // eslint-disable-next-line
-      console.error(
-        'Error: input must be a 4-digit number with unique digits.',
-      );
+      console.error('Error: input must be a 4-digit number with unique digits.');
       continue;
     }
 
     const { bulls } = getBullsAndCows(secret, input);
 
     if (bulls === 4) {
+      // console.log(`Congratulations! You guessed the number: ${secret}`);
       isGuessed = true;
     } else {
+      // console.log(`Bulls: ${bulls}, Cows: ${cows}`);
     }
   }
 }
